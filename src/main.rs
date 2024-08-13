35cdfec36d8d59c0142688f6b1bed165b259c3a5
Sammy-ch <sammystcherubin@gmail.com>
2024-08-13 17:42:43 +0200

    init commit

---
+++ src/main.rs
@@ -0,0 +1,32 @@
+use std::env;
+use std::process;
+
+
+use minigrep::Config;
+use minigrep::run;
+
+fn main() {
+    
+
+    let config = Config::build(
+        env::args()
+    ).unwrap_or_else(|err: &str| {
+        println!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
+
+
+
+
+if let Err(e) = run(config)  {
+    println!("Application error: {e}")
+}
+
+}
+
+
+
+
+
+
+
